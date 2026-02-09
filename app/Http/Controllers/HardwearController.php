9fe2d4cfbb0f07850bf5173fb9883990ce4288e1
jesse <jessespruit@gmail.com>
2026-02-09 11:39:30 +0100

    Implement Hardwear management features: add create, store, and destroy methods in HardwearController; define Hardwearmodel with fillable attributes.

--- app/Http/Controllers/HardwearController.php
+++ app/Http/Controllers/HardwearController.php
@@ -4,10 +4,58 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Hardwearmodel;
+
 class HardwearController extends Controller
 {
-    index()
+    public function index()
     {
-        return view('home');
+        return view('hardwear.index', [
+            'hardwear' => Hardwearmodel::orderBy('naam')->get()
+        ]);
+    }
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('hardwear.create');
+    }
+
+
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'naam' => 'required',
+            'aantal' => 'required|integer',
+            'prijs' => 'required|numeric',
+        ]);
+
+        Hardwearmodel::create($validated);
+
+        return redirect()->route('hardwear.index')->with('success', 'Hardwear created successfully.');
+    }
+    public function show(string $id)
+    {
+        //
+    }
+
+
+
+    public function edit()
+    {
+     
+    }
+    public function update(Request $request, )
+    {
+        
+
+    }
+
+    public function destroy(Hardwearmodel $hardwear)
+    {
+        $hardwear->delete();
+        return redirect()->route('hardwear.index')->with('success', 'Hardwear verwijderd!');
     }
 }
---
+++ app/Models/Hardwearmodel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Hardwearmodel extends Model
+{
+    protected $fillable = ['naam', 'aantal', 'prijs'];
+    
+}
