08ba0482a4302823edfa7a00c88419bf4071ac26
jesse <jessespruit@gmail.com>
2026-02-09 11:14:51 +0100

    Add routes for dashboard and home with authentication middleware

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('/home', function () {
+    return view('home');
+})->middleware(['auth'])->name('home');
